7bb787653c0824c732f16a2f5f41f39dff0f7fc9
SaidTheCoder <71198810+SaidTheCoder@users.noreply.github.com>
2021-04-01 20:48:11 -0400

    Add files via upload

---
+++ digit.py
@@ -0,0 +1,8 @@
+def countDigitsFromNumber():
+    number=input("enter your number here:   ")
+    count = 0
+    for s in number:
+        count=count+1
+        print("the number of digits in this number are  ",count)
+countDigitsFromNumber()
+
